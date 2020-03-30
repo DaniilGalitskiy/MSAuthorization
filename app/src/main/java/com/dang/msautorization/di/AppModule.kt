f418753314f09433098b664ca7cc3accd8fb16f9
Daniil Galitskiy <Olfethor@gmail.com>
2020-03-30 18:34:34 +0300

    testLast

--- app/src/main/java/com/dang/msautorization/di/AppModule.kt
+++ app/src/main/java/com/dang/msautorization/di/AppModule.kt
@@ -17,8 +17,8 @@ import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
+import java.util.concurrent.TimeUnit
 
 @Module
 class AppModule {
